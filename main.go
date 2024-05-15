62de1f03d95f4ad76bc29efdec6bf25e1dde93fc
aaygun <aliaygun365@gmail.com>
2024-05-15 14:30:31 +0300

    update

--- main.go
+++ main.go
@@ -37,12 +37,11 @@ func find_start_end_comment(data string) (int, int, int, int, []string) {
 	return result_start, result_end, result_comment, result_actions, sentences
 }
 
-func save_data(start, end, comment, action int, sentences []string) ([]string, []string, []string, []string, []string) {
+func save_data(start, end, comment, action int, sentences []string) ([]string, []string, []string, []string) {
 	start_rows := []string{}
 	end_rows := []string{}
 	comment_rows := []string{}
 	connect_rows := []string{}
-	action_rows := []string{}
 	for i, row := range sentences {
 		if i > start && i < end {
 			row = string(row[0])
@@ -62,9 +61,6 @@ func save_data(start, end, comment, action int, sentences []string) ([]string, [
 			if i > end+1 && i < action {
 				connect_rows = append(connect_rows, row)
 			}
-			if i >= action {
-				action_rows = append(action_rows, row)
-			}
 		} else {
 			if i > end+1 {
 				connect_rows = append(connect_rows, row)
@@ -72,7 +68,7 @@ func save_data(start, end, comment, action int, sentences []string) ([]string, [
 		}
 	}
 
-	return start_rows, comment_rows, end_rows, connect_rows, action_rows
+	return start_rows, comment_rows, end_rows, connect_rows
 }
 
 func read_file(file_path string) string {
@@ -116,39 +112,68 @@ func find_connection(current_room string, connect_rows []string) []string {
 	return result
 }
 
-func find_road_options(start_rows, end_rows, connect_rows []string) {
-	for _, row := range start_rows {
-		connect_rows := find_connection(row, connect_rows)
-		for _, connect_row := range connect_rows {
-			fmt.Println("Room: ", row, " Connect Rows: ", connect_row)
+func check_end_room(option_rooms []string, end_rows []string) bool {
+	for _, room := range option_rooms {
+		for _, end_room := range end_rows {
+			if room == end_room {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func loop_handler(room string, road []string) bool {
+	for _, r := range road {
+		if r == room {
+			return false
+		}
+	}
+	return true
+}
+func find_road_recursive(room string, start_rows, end_rows, connect_rows, comment_rows []string, road []string, roads *[][]string) {
+	if check_end_room([]string{room}, end_rows) {
+		*roads = append(*roads, append(road, room))
+		return
+	}
+	connect_rooms := find_connection(room, connect_rows)
+	for _, next_room := range connect_rooms {
+		if loop_handler(next_room, road) {
+			find_road_recursive(next_room, start_rows, end_rows, connect_rows, comment_rows, append(road, room), roads)
 		}
 	}
 }
 
+func find_road_options_recursive(start_rows, end_rows, comment_rows, connect_rows []string) [][]string {
+	roads := [][]string{}
+	for _, room := range start_rows {
+		find_road_recursive(room, start_rows, end_rows, connect_rows, comment_rows, []string{}, &roads)
+	}
+	return roads
+}
+
 func main() {
 	content := read_file("map_example_1.txt")
-	var start_rows, comment_rows, end_rows, connect_rows, actions_rows []string
-	start_rows, comment_rows, end_rows, connect_rows, actions_rows = save_data(find_start_end_comment(content))
-	fmt.Println("Start Rows: ")
-	for _, row := range start_rows {
-		fmt.Println(row)
-	}
-	fmt.Println("Comment Rows: ")
-	for _, row := range comment_rows {
-		fmt.Println(row)
-	}
-	fmt.Println("End Rows: ")
-	for _, row := range end_rows {
-		fmt.Println(row)
-	}
-	fmt.Println("Connect Rows: ")
-	for _, row := range connect_rows {
-		fmt.Println(row)
-	}
-	fmt.Println("Actions Rows: ")
-	for _, row := range actions_rows {
-		fmt.Println(row)
-	}
-	//fmt.Println(find_connection("2", connect_rows))
-	find_road_options(start_rows, end_rows, connect_rows)
+	var start_rows, comment_rows, end_rows, connect_rows []string
+	start_rows, comment_rows, end_rows, connect_rows = save_data(find_start_end_comment(content))
+	/*
+		fmt.Println("Start Rows: ")
+		for _, row := range start_rows {
+			fmt.Println(row)
+		}
+		fmt.Println("Comment Rows: ")
+		for _, row := range comment_rows {
+			fmt.Println(row)
+		}
+		fmt.Println("End Rows: ")
+		for _, row := range end_rows {
+			fmt.Println(row)
+		}
+		fmt.Println("Connect Rows: ")
+		for _, row := range connect_rows {
+			fmt.Println(row)
+		}
+	*/
+	// fmt.Println(find_connection("2", connect_rows))
+	fmt.Println(find_road_options_recursive(start_rows, end_rows, comment_rows, connect_rows))
 }
